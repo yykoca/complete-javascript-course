6c76b1cc949122fb7a4c2e7e6a4e562e3b4d2457
yykoca <yykoca.08@gmail.com>
2021-04-02 23:10:33 +0200

    displayMessage function add

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -13,36 +13,39 @@ console.log(document.querySelector(".guess").value);
 document.querySelector(".guess").value = 23;
 */
 
-const number = Math.ceil(Math.random()*20);
+const number = Math.ceil(Math.random() * 20);
 let score = 20;
 console.log(number);
 document.querySelector('.check').addEventListener('click', checkNumber);
 document.addEventListener('keydown', (e) => {
-    if (e.code === "Enter"){
-        checkNumber()   
+    if (e.code === "Enter") {
+        checkNumber()
     }
 });
 
-function checkNumber(){
+function displayMessage(message) {
+    document.querySelector(".message").textContent = message;
+}
+
+function checkNumber() {
     const guessNumber = Number(document.querySelector(".guess").value);
-    
-    if(guessNumber === 0){
-        document.querySelector(".message").textContent = "⛔️ No Number";
+
+    if (guessNumber === 0) {
+        displayMessage("⛔️ No Number");
     } else if (guessNumber === number) {
-        document.querySelector(".message").textContent = "🎉 Correct";
         document.querySelector(".highscore").textContent = score;
-        
+        displayMessage("🎉 Correct Number!");
+
         document.querySelector(".number").textContent = number;
         document.querySelector(".number").style.width = "25rem";
         document.querySelector("body").style.backgroundColor = "green";
-    } 
-    else if (guessNumber > number) {
-        document.querySelector(".message").textContent = "📈 too high";
+    } else if (guessNumber > number) {
         score--;
         document.querySelector(".score").textContent = score;
+        displayMessage("📈 Too high !!");
     } else if (guessNumber < number) {
-        document.querySelector(".message").textContent = "📉 too low";
         score--;
         document.querySelector(".score").textContent = score;
+        displayMessage("📉 Too low !!");
     }
 }
